9162ae17b7f220aa74b2572f3d97787ec031695e
DoukheeWon <fain9301@yahoo.com>
2024-04-19 01:03:32 +0900

    feat: first commit

---
+++ src/main/java/kr/co/won/udemyspringmongomvc/UdemySpringMongoMvcApplication.java
@@ -0,0 +1,13 @@
+package kr.co.won.udemyspringmongomvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UdemySpringMongoMvcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UdemySpringMongoMvcApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kr/co/won/udemyspringmongomvc/UdemySpringMongoMvcApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.co.won.udemyspringmongomvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UdemySpringMongoMvcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
